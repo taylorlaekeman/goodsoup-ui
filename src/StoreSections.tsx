774d3665c27117044a923a6d520698adac99bf37
taylor <taylor.laekeman@gmail.com>
2026-02-22 11:03:33 -0500

    add ability to create section

--- src/StoreSections.tsx
+++ src/StoreSections.tsx
@@ -1,12 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { graphql } from './gql';
 import { gqlClient } from './gqlClient';
 
 export function StoreSections() {
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: ['getSections'],
     queryFn: async () => gqlClient.request(getSectionsDocument),
   });
+
+  const [isCreating, setIsCreating] = useState(false);
+  const [newSection, setNewSection] = useState('');
+
+  const { mutate: createSection } = useMutation({
+    mutationFn: async (name: string) =>
+      gqlClient.request(CREATE_SECTION_DOCUMENT, { name }),
+    onSuccess: () => {
+      refetch();
+      setNewSection('');
+      setIsCreating(false);
+    },
+  });
+
   return (
     <>
       <h2>Sections</h2>
@@ -15,6 +30,18 @@ export function StoreSections() {
           <li key={section.id}>{section.name}</li>
         ))}
       </ul>
+      {isCreating ? (
+        <>
+          <input
+            onChange={(event) => setNewSection(event.target.value)}
+            value={newSection}
+          />
+          <button onClick={() => createSection(newSection)}>Save</button>
+          <button onClick={() => setIsCreating(false)}>Cancel</button>
+        </>
+      ) : (
+        <button onClick={() => setIsCreating(true)}>+ New</button>
+      )}
     </>
   );
 }
@@ -22,6 +49,15 @@ export function StoreSections() {
 const getSectionsDocument = graphql(/* GraphQL */ `
   query GetSections {
     sections {
+      id
+      name
+    }
+  }
+`);
+
+const CREATE_SECTION_DOCUMENT = graphql(/* GraphQL */ `
+  mutation CreateSection($name: String!) {
+    createSection(name: $name) {
       id
       name
     }
